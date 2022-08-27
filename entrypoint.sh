c6fd6fdbbb996565fa331ab21fed82ca98b375f0
Joao-Moura <jaummoura15@gmail.com>
2022-08-26 21:09:46 -0300

    Subindo yml de configuração do heroku para o backend

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,9 @@ if [ "$1" = "run-back" ]; then
     cd library_back
     if [ "$2" = "django-debug" ]; then
         exec python manage.py runserver 0.0.0.0:8000
-    elif [ "$2" = "django-gunicorn" ]; then
+    elif [ "$2" = "django-migrate" ]; then
         python manage.py migrate
         python manage.py collectstatic --noinput
-        exec gunicorn "src.wsgi:application" \
-            --bind 0.0.0.0:8000 \
-            --workers 4
     fi
 elif [ "$1" = "run-front" ]; then
     cd library_front
